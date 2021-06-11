618c731200cb592d9a8c1bd4c82744d293d4f735
Amixir <alejopq639@gmail.com>
2021-06-11 13:46:54 -0500

    inicio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = inicio;
+
+function inicio(){
+	var nombre = prompt("ingrese su nombre");                                   
+	alert("hola "+nombre);  
+	console.log("Hola Mundo 2");
+} 
